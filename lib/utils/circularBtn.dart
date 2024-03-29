a107e067e17a33e9bf0ffbb6009c723a7fa291ae
chandan0000 <kumarchandan41u@gmail.com>
2024-03-29 08:05:58 +0530

    flutter webrtc

---
+++ lib/utils/circularBtn.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CircularBtn extends StatelessWidget {
+  const CircularBtn(
+      {super.key,
+      required this.color,
+      required this.icon,
+      required this.onPress});
+
+  final Color color;
+  final Widget icon;
+  final VoidCallback onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipOval(
+      child: Material(
+        color: color, // Button color
+        child: InkWell(
+          // splashColor: Colors.red, // Splash color
+          onTap: onPress,
+          child: SizedBox(width: 56, height: 56, child: icon),
+        ),
+      ),
+    );
+  }
+}
